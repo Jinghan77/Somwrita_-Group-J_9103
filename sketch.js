5abf677789dc87f9b216ace896474e8daa0a223e
JiShi-usyd <jshi0588@uni.sydney.edu.au>
2025-06-03 10:50:11 +1000

    Create Elements
    Create a canvas with a width and height of 400, set the canvas background to black, set the stroke color of all rectangles to black and the size to 6 pixels

--- sketch.js
+++ sketch.js
@@ -3,3 +3,11 @@ let colors = ['red', 'blue', 'yellow'];
 let rectWidths = [40, 50, 60, 70, 80, 90];
 let rectHeights = [30, 40, 50];
 
+function setup(){
+  createCanvas(canvasSize, canvasSize);
+  background(0);
+  stroke(0);
+  strokeWeight(6);
+
+  
+}
