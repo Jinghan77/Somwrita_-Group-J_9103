e151e21dd45dbdecf56cbca2a148ce9e7ba7bef3
Yudian Hou <yhou0161@uni.sydney.edu.au>
2025-06-04 22:08:11 +1000

    Update sketch.js
    Draw Path

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 let canvasSize = 400;
-let colors = ['red', 'blue', 'yellow'];
+let colors = ['red', 'blue', 'yellow', 'White'];
 let rectWidths = [40, 50, 60, 70, 80, 90];
 let rectHeights = [30, 40, 50];
 
@@ -31,4 +31,37 @@ function setup(){
     }
     y += h;
   }
+  drawPath();
+}
+
+function drawPath(){
+  stroke(0);
+  strokeWeight(30);
+  noFill();
+  
+  //Horizon Path
+  line(25, 25, 180, 25);
+  line(260, 25, 375, 25);
+  line(25, 100, 260, 100);
+  line(230, 175, 375, 175);
+  line(140, 175, 180, 175);
+  line(100, 220, 290, 220);
+  line(25, 270, 100, 270);
+  line(230, 270, 375, 270);
+  line(25, 375, 180, 375);
+  line(290, 375, 375, 375);
+
+  //Vertical Path
+  line(25, 25, 25, 100);
+  line(260, 25, 260, 175);
+  line(375, 25, 375, 175);
+  line(180, 25, 180, 100);
+  line(140, 100, 140, 175);
+  line(180, 175, 180, 220);
+  line(180, 175, 180, 375);
+  line(230, 175, 230, 270);
+  line(100, 220, 100, 270);
+  line(290, 220, 290, 375);
+  line(25, 270, 25, 375);
+  line(375, 270, 375, 375);
 }
