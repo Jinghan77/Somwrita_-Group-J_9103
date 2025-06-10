58be43d32ae6f1f62c98d5017f28a13b311a37ca
Yudian Hou <yhou0161@uni.sydney.edu.au>
2025-06-10 12:47:56 +1000

    update machine sketch.js
    stay at the middle of the window

--- 9103-hfan0348/sketch.js
+++ 9103-hfan0348/sketch.js
@@ -1,4 +1,5 @@
-let offsetX = 40;
+let offsetX = -170;
+let offsetY = -400;
 let pixelFont;
 let pointsPacman = [];
 let pointsPiet = [];
@@ -8,262 +9,248 @@ function preload() {
 }
 
 function setup() { 
-  createCanvas(680, 1500);
+  createCanvas(windowWidth, windowHeight);
   noStroke();
   textFont(pixelFont);
   fill(255);
   textAlign(CENTER);
 
-  pointsPacman = pixelFont.textToPoints("Pacman", 130, 70, 100, {
+  pointsPacman = pixelFont.textToPoints("Pacman", 130 + offsetX, 70, 100, {
     sampleFactor: 0.12,
   });
-  pointsPiet = pixelFont.textToPoints("* PIET", 270, 180, 100, {
+  pointsPiet = pixelFont.textToPoints("* PIET", 270 + offsetX, 180, 100, {
     sampleFactor: 0.12,
   });
   pixelDensity(2);
 }
 
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
+}
+
 function draw() {
   // background
   background(255, 250, 205); 
-
-  translate(0, 120);
-
-  textFont(pixelFont);
-  fill(255);
-  textSize(16);
-  text("Click the screen to start the music", 0, height - 30);
-
-  // black (机身)
-  fill(0, 0, 0);
-  rect(38 + offsetX, -30, 520, 220); 
-  rect(48 + offsetX, 220, 500, 500); 
-  rect(28 + offsetX, 200, 540, 10); 
-  rect(28 + offsetX, -60, 540, 10); 
-  rect(28 + offsetX, -60, 10, 260); 
-  rect(558 + offsetX, -60, 10, 260); 
-  rect(38 + offsetX, 210, 520, 10); 
-  rect(10 + offsetX, -80, 30, 10); 
-  rect(558 + offsetX, -80, 30, 10); 
-  rect(28 + offsetX, -70, 20, 10); 
-  rect(548 + offsetX, -70, 20, 10); 
-  rect(2 + offsetX, -70, 8, 280); 
-  rect(588 + offsetX, -70, 8, 280); 
-  rect(8 + offsetX, 210, 10, 10);
-  rect(578 + offsetX, 210, 10, 10);
-  rect(18 + offsetX, 220, 10, 510);
-  rect(568 + offsetX, 220, 10, 510);
-  rect(10 + offsetX, 730, 8, 20);
-  rect(578 + offsetX, 730, 8, 30);
-  rect(40 + offsetX, 720, 516, 20);
-  rect(50, 730, 8, 30);
-  rect(42, 760, 8, 30);
-  rect(626, 760, 8, 30);
-  rect(34, 790, 8, 30);
-  rect(634, 790, 8, 30);
-  rect(34, 790, 8, 30);
-  rect(634, 790, 8, 30);
-  rect(26, 820, 8, 110);
-  rect(642, 820, 8, 110);
-  rect(48, 840, 580, 90);
-  rect(56, 930, 564, 20);
-  rect(72, 950, 532, 20);
-  rect(80, 970, 516, 220);
   
-  // dark grey #3E3A39
-  fill(62, 58, 57);
-  rect(58, 740, 560, 110);
+  push();
+  translate(width / 2, height / 2);
+  translate(offsetX, offsetY);
 
-  // light red #DA3C34
-  fill(218, 60, 52);
-  rect(38 + offsetX, -50, 520, 20);
-  rect(10 + offsetX, -70, 20, 270);
-  rect(568 + offsetX, -70, 20, 270);
-  rect(28 + offsetX, 220, 20, 500);
-  rect(548 + offsetX, 220, 20, 500);
-  rect(18 + offsetX, 730, 22, 20);
-  rect(556 + offsetX, 730, 22, 30);
-  rect(28 + offsetX, 720, 12, 10);
-  rect(556 + offsetX, 720, 12, 10);
-  rect(58, 730, 14, 30);
-  rect(50, 760, 14, 30);
-  rect(612, 760, 14, 30);
-  rect(42, 790, 14, 30);
-  rect(620, 790, 14, 30);
-  rect(34, 820, 14, 110);
-  rect(628, 820, 14, 110);
-  rect(58, 970, 22, 220);
-  rect(596, 970, 22, 220);
-  rect(221, 980, 10, 150);
-  rect(431, 980, 10, 150);
-  rect(231, 1130, 200, 10);
-  
-  // red #9C1F24
-  fill(156, 31, 36);
-  rect(38 + offsetX, 190, 520, 10);
-  rect(22 + offsetX, -70, 8, 270);
-  rect(568 + offsetX, -70, 8, 270);
-  rect(10 + offsetX, 200, 20, 10);
-  rect(568 + offsetX, 200, 20, 10);
-  rect(40 + offsetX, 220, 8, 500);
-  rect(548 + offsetX, 220, 8, 500);
-  rect(34, 930, 14, 20);
-  rect(628, 930, 14, 20);
-  rect(50, 950, 14, 20);
-  rect(612, 950, 14, 20);
-  rect(72, 970, 8, 220);
-  rect(596, 970, 8, 220);
-  rect(58, 970, 14, 50);
-  rect(604, 970, 14, 50);
-  rect(58, 1170, 14, 20);
-  rect(604, 1170, 14, 20);
-  rect(221, 1010, 10, 10);
-  rect(431, 1010, 10, 10);
+  drawBody();
+  drawScreen();
+  pop();
 
-  // heavy red #631018
-  fill(99, 16, 24);
-  rect(18 + offsetX, 210, 20, 10);
-  rect(558 + offsetX, 210, 20, 10);
-  rect(72, 730, 8, 20);
-  rect(596, 730, 8, 20);
-  rect(64, 760, 8, 20);
-  rect(604, 760, 8, 20);
-  rect(56, 790, 8, 20);
-  rect(612, 790, 8, 20);
-  rect(48, 820, 8, 20);
-  rect(620, 820, 8, 20);
-  rect(34, 850, 14, 80);
-  rect(628, 850, 14, 80);
-  rect(48, 930, 14, 20);
-  rect(614, 930, 14, 20);
-  rect(56, 950, 16, 20);
-  rect(604, 950, 16, 20);
-  rect(72, 970, 8, 50);
-  rect(596, 970, 8, 50);
-  rect(231, 970, 200, 10);
-  rect(221, 980, 10, 30);
-  rect(431, 980, 10, 30);
-
-  // dark grey #3E3A39
-  fill(62, 58, 57);
-  rect(98 + offsetX, 250, 400, 10);
-  rect(88 + offsetX, 260, 10, 10);
-  rect(498 + offsetX, 260, 10, 10);
-  rect(78 + offsetX, 270, 10, 400);
-  rect(508 + offsetX, 270, 10, 400);
-  rect(88 + offsetX, 670, 10, 10);
-  rect(498 + offsetX, 670, 10, 10);
-  rect(98 + offsetX, 680, 400, 10);
-  rect(48 + offsetX, 720, 500, 10);
-  rect(251, 1000, 160, 120);
-
-  //black
-  fill(0, 0, 0);
-  rect(72, 750, 8, 30);
-  rect(596, 750, 8, 30);
-  rect(64, 780, 8, 30);
-  rect(604, 780, 8, 30);
-  rect(56, 810, 8, 30);
-  rect(612, 810, 8, 30);
-  rect(34, 930, 8, 20);
-  rect(634, 930, 8, 20);
-  rect(42, 950, 8, 20);
-  rect(626, 950, 8, 20);
-  rect(50, 970, 8, 220);
-  rect(618, 970, 8, 220);
-  rect(58, 1190, 38, 20);
-  rect(580, 1190, 38, 20);
-  rect(372, 1000, 40, 60);
-  rect(272, 1052, 40, 40);
-
-  //button
-  //black
-  fill(0, 0, 0);
-  rect(492, 750, 40, 48);
-  rect(484, 758, 56, 32);
-  rect(500, 742, 24, 64);
-
-  // light red #DA3C34
-  fill(218, 60, 52);
-  rect(380, 1010, 20, 40);
-  rect(280, 1060, 20, 20);
-
-  rect(500, 750, 24, 40);
-  rect(492, 758, 40, 24);
-  // red #9C1F24
-  fill(156, 31, 36);
-  rect(492, 782, 8, 8);
-  rect(524, 782, 8, 8);
-  rect(500, 790, 24, 8);
-  //highlight
-  fill(255);
-  rect(500, 758, 8, 8);
-  
-    //yellow button
-    //black
-    fill(0, 0, 0);
-    rect(412, 770, 40, 48);
-    rect(420, 762, 24, 64);
-    rect(404, 778, 56, 32);
-    // gold #FFD700
-    fill(255, 215, 0);
-    rect(420, 770, 24, 40);
-    rect(412, 778, 40, 24);
-    // gold #FFA500
-    fill(255, 165, 0);
-    rect(412, 802, 8, 8);
-    rect(444, 802, 8, 8);
-    rect(420, 810, 24, 8);
-    //highlight
-    fill(255);
-    rect(420, 780, 10, 10);
-
-    // 摇杆放大版（1.5x 缩放）
-    let scale = 1.5;
-    let baseX = 114;
-    let baseY = 708;
-
-    // black
-    fill(0, 0, 0);
-    rect(baseX, baseY, 56 * scale, 32 * scale);
-    rect(baseX + 8 * scale, baseY + 32 * scale, 40 * scale, 8 * scale);
-    rect(baseX + 16 * scale, baseY + 40 * scale, 24 * scale, 30 * scale);
-    rect(baseX + 8 * scale, baseY + 54 * scale, 40 * scale, 24 * scale);
-    rect(baseX + 16 * scale, baseY + 78 * scale, 24 * scale, 8 * scale);
-    rect(baseX, baseY + 62 * scale, 56 * scale, 8 * scale);
-
-    // dark grey #3E3A39
-    fill(62, 58, 57);
-    rect(baseX + 24 * scale, baseY + 48 * scale, 8 * scale, 14 * scale);
-    rect(baseX + 16 * scale, baseY + 70 * scale, 24 * scale, 8 * scale);
-    rect(baseX + 8 * scale, baseY + 62 * scale, 8 * scale, 8 * scale);
-    rect(baseX + 40 * scale, baseY + 62 * scale, 8 * scale, 8 * scale);
-
-    // Darker Blue #0056B3
-    fill(0, 86, 179);
-    rect(baseX + 8 * scale, baseY + 14 * scale, 40 * scale, 18 * scale);
-    rect(baseX + 16 * scale, baseY + 32 * scale, 24 * scale, 8 * scale);
-
-    // blue #007BFF
-    fill(0, 123, 255);
-    rect(baseX + 16 * scale, baseY - 8 * scale, 24 * scale, 40 * scale);
-    rect(baseX + 8 * scale, baseY, 40 * scale, 14 * scale);
-
-    // highlight
-    fill(255);
-    rect(baseX + 16 * scale, baseY, 8 * scale, 8 * scale);
-
-  //highlight
-  fill(98, 99, 99);
-  rect(56, 840, 562, 10);  
-  
-  
-  //screen
   fill(255, 255, 255);
-  rect(98 + offsetX, 270, 400, 400);
+  rect(width / 2 - 310, height - 70, 620, 50)
+
+  fill(0);
+  textSize(32);
+  text("Click the screen to start the music", width / 2, height - 30);
+}
+
+function drawScreen() {
+  fill(255, 255, 255);
+  rect(138 + offsetX, 270, 400, 400);
   let brightness = 255;
   drawNeonText(pointsPacman, brightness);
   drawNeonText(pointsPiet, brightness);
+}
+
+function drawBody(){
+  // black (机身)
+    fill(0, 0, 0);
+  rect(78 + offsetX, -30, 520, 220);
+  rect(88 + offsetX, 220, 500, 500);
+  rect(68 + offsetX, 200, 540, 10);
+  rect(68 + offsetX, -60, 540, 10);
+  rect(68 + offsetX, -60, 10, 260);
+  rect(598 + offsetX, -60, 10, 260);
+  rect(78 + offsetX, 210, 520, 10);
+  rect(50 + offsetX, -80, 30, 10);
+  rect(598 + offsetX, -80, 30, 10);
+  rect(68 + offsetX, -70, 20, 10);
+  rect(588 + offsetX, -70, 20, 10);
+  rect(42 + offsetX, -70, 8, 280);
+  rect(628 + offsetX, -70, 8, 280);
+  rect(48 + offsetX, 210, 10, 10);
+  rect(618 + offsetX, 210, 10, 10);
+  rect(58 + offsetX, 220, 10, 510);
+  rect(608 + offsetX, 220, 10, 510);
+  rect(50 + offsetX, 730, 8, 20);
+  rect(618 + offsetX, 730, 8, 30);
+  rect(80 + offsetX, 720, 516, 20);
+  rect(50 + offsetX, 730, 8, 30);
+  rect(42 + offsetX, 760, 8, 30);
+  rect(626 + offsetX, 760, 8, 30);
+  rect(34 + offsetX, 790, 8, 30);
+  rect(634 + offsetX, 790, 8, 30);
+  rect(26 + offsetX, 820, 8, 110);
+  rect(642 + offsetX, 820, 8, 110);
+  rect(48 + offsetX, 840, 580, 90);
+  rect(56 + offsetX, 930, 564, 20);
+  rect(72 + offsetX, 950, 532, 20);
+  rect(80 + offsetX, 970, 516, 220);
+
+  // dark grey
+  fill(62, 58, 57);
+  rect(58 + offsetX, 740, 560, 110);
+
+  // light red
+  fill(218, 60, 52);
+  rect(78 + offsetX, -50, 520, 20);
+  rect(50 + offsetX, -70, 20, 270);
+  rect(608 + offsetX, -70, 20, 270);
+  rect(68 + offsetX, 220, 20, 500);
+  rect(588 + offsetX, 220, 20, 500);
+  rect(58 + offsetX, 730, 22, 20);
+  rect(596 + offsetX, 730, 22, 30);
+  rect(68 + offsetX, 720, 12, 10);
+  rect(596 + offsetX, 720, 12, 10);
+  rect(58 + offsetX, 730, 14, 30);
+  rect(50 + offsetX, 760, 14, 30);
+  rect(612 + offsetX, 760, 14, 30);
+  rect(42 + offsetX, 790, 14, 30);
+  rect(620 + offsetX, 790, 14, 30);
+  rect(34 + offsetX, 820, 14, 110);
+  rect(628 + offsetX, 820, 14, 110);
+  rect(58 + offsetX, 970, 22, 220);
+  rect(596 + offsetX, 970, 22, 220);
+  rect(221 + offsetX, 980, 10, 150);
+  rect(431 + offsetX, 980, 10, 150);
+  rect(231 + offsetX, 1130, 200, 10);
+
+  // red
+  fill(156, 31, 36);
+  rect(78 + offsetX, 190, 520, 10);
+  rect(62 + offsetX, -70, 8, 270);
+  rect(608 + offsetX, -70, 8, 270);
+  rect(50 + offsetX, 200, 20, 10);
+  rect(608 + offsetX, 200, 20, 10);
+  rect(80 + offsetX, 220, 8, 500);
+  rect(588 + offsetX, 220, 8, 500);
+  rect(34 + offsetX, 930, 14, 20);
+  rect(628 + offsetX, 930, 14, 20);
+  rect(50 + offsetX, 950, 14, 20);
+  rect(612 + offsetX, 950, 14, 20);
+  rect(72 + offsetX, 970, 8, 220);
+  rect(596 + offsetX, 970, 8, 220);
+  rect(58 + offsetX, 970, 14, 50);
+  rect(604 + offsetX, 970, 14, 50);
+  rect(58 + offsetX, 1170, 14, 20);
+  rect(604 + offsetX, 1170, 14, 20);
+  rect(221 + offsetX, 1010, 10, 10);
+  rect(431 + offsetX, 1010, 10, 10);
+
+  // heavy red
+  fill(99, 16, 24);
+  rect(58 + offsetX, 210, 20, 10);
+  rect(598 + offsetX, 210, 20, 10);
+  rect(72 + offsetX, 730, 8, 20);
+  rect(596 + offsetX, 730, 8, 20);
+  rect(64 + offsetX, 760, 8, 20);
+  rect(604 + offsetX, 760, 8, 20);
+  rect(56 + offsetX, 790, 8, 20);
+  rect(612 + offsetX, 790, 8, 20);
+  rect(48 + offsetX, 820, 8, 20);
+  rect(620 + offsetX, 820, 8, 20);
+  rect(34 + offsetX, 850, 14, 80);
+  rect(628 + offsetX, 850, 14, 80);
+  rect(48 + offsetX, 930, 14, 20);
+  rect(614 + offsetX, 930, 14, 20);
+  rect(56 + offsetX, 950, 16, 20);
+  rect(604 + offsetX, 950, 16, 20);
+  rect(72 + offsetX, 970, 8, 50);
+  rect(596 + offsetX, 970, 8, 50);
+  rect(231 + offsetX, 970, 200, 10);
+  rect(221 + offsetX, 980, 10, 30);
+  rect(431 + offsetX, 980, 10, 30);
+
+  // dark grey detail
+  fill(62, 58, 57);
+  rect(138 + offsetX, 250, 400, 10);
+  rect(128 + offsetX, 260, 10, 10);
+  rect(538 + offsetX, 260, 10, 10);
+  rect(118 + offsetX, 270, 10, 400);
+  rect(548 + offsetX, 270, 10, 400);
+  rect(128 + offsetX, 670, 10, 10);
+  rect(538 + offsetX, 670, 10, 10);
+  rect(138 + offsetX, 680, 400, 10);
+  rect(88 + offsetX, 720, 500, 10);
+  rect(251 + offsetX, 1000, 160, 120);
+
+  // black buttons
+  fill(0, 0, 0);
+  rect(72 + offsetX, 750, 8, 30);
+  rect(596 + offsetX, 750, 8, 30);
+  rect(64 + offsetX, 780, 8, 30);
+  rect(604 + offsetX, 780, 8, 30);
+  rect(56 + offsetX, 810, 8, 30);
+  rect(612 + offsetX, 810, 8, 30);
+  rect(34 + offsetX, 930, 8, 20);
+  rect(634 + offsetX, 930, 8, 20);
+  rect(42 + offsetX, 950, 8, 20);
+  rect(626 + offsetX, 950, 8, 20);
+  rect(50 + offsetX, 970, 8, 220);
+  rect(618 + offsetX, 970, 8, 220);
+  rect(58 + offsetX, 1190, 38, 20);
+  rect(580 + offsetX, 1190, 38, 20);
+  rect(372 + offsetX, 1000, 40, 60);
+  rect(272 + offsetX, 1052, 40, 40);
+
+  // red button
+  fill(0);
+  rect(492 + offsetX, 750, 40, 48);
+  rect(484 + offsetX, 758, 56, 32);
+  rect(500 + offsetX, 742, 24, 64);
+  fill(218, 60, 52);
+  rect(500 + offsetX, 750, 24, 40);
+  rect(492 + offsetX, 758, 40, 24);
+  fill(156, 31, 36);
+  rect(492 + offsetX, 782, 8, 8);
+  rect(524 + offsetX, 782, 8, 8);
+  rect(500 + offsetX, 790, 24, 8);
+  fill(255);
+  rect(500 + offsetX, 758, 8, 8);
+
+  // yellow button
+  fill(0);
+  rect(412 + offsetX, 770, 40, 48);
+  rect(420 + offsetX, 762, 24, 64);
+  rect(404 + offsetX, 778, 56, 32);
+  fill(255, 215, 0);
+  rect(420 + offsetX, 770, 24, 40);
+  rect(412 + offsetX, 778, 40, 24);
+  fill(255, 165, 0);
+  rect(412 + offsetX, 802, 8, 8);
+  rect(444 + offsetX, 802, 8, 8);
+  rect(420 + offsetX, 810, 24, 8);
+  fill(255);
+  rect(420 + offsetX, 780, 10, 10);
+
+  // joystick (enlarged 1.5x)
+  let scale = 1.5;
+  let baseX = 140 + offsetX;
+  let baseY = 708;
+  fill(0);
+  rect(baseX, baseY, 56 * scale, 32 * scale);
+  rect(baseX + 8 * scale, baseY + 32 * scale, 40 * scale, 8 * scale);
+  rect(baseX + 16 * scale, baseY + 40 * scale, 24 * scale, 30 * scale);
+  rect(baseX + 8 * scale, baseY + 54 * scale, 40 * scale, 24 * scale);
+  rect(baseX + 16 * scale, baseY + 78 * scale, 24 * scale, 8 * scale);
+  rect(baseX, baseY + 62 * scale, 56 * scale, 8 * scale);
+  fill(62, 58, 57);
+  rect(baseX + 24 * scale, baseY + 48 * scale, 8 * scale, 14 * scale);
+  rect(baseX + 16 * scale, baseY + 70 * scale, 24 * scale, 8 * scale);
+  rect(baseX + 8 * scale, baseY + 62 * scale, 8 * scale, 8 * scale);
+  rect(baseX + 40 * scale, baseY + 62 * scale, 8 * scale, 8 * scale);
+  fill(0, 86, 179);
+  rect(baseX + 8 * scale, baseY + 14 * scale, 40 * scale, 18 * scale);
+  rect(baseX + 16 * scale, baseY + 32 * scale, 24 * scale, 8 * scale);
+  fill(0, 123, 255);
+  rect(baseX + 16 * scale, baseY - 8 * scale, 24 * scale, 40 * scale);
+  rect(baseX + 8 * scale, baseY, 40 * scale, 14 * scale);
+  fill(255);
+  rect(baseX + 16 * scale, baseY, 8 * scale, 8 * scale); 
 }
 
 function drawNeonText(points, brightness, offsetX = 0, offsetY = 0) {
